3754be6df97d9a36c13b6be39b22888584ab7c50
Austin Chandra <austinchandra@berkeley.edu>
2023-03-05 02:28:13 -0700

    Add Axelar backup store

--- truffle-config.js
+++ truffle-config.js
@@ -44,8 +44,7 @@
 require('dotenv').config();
 const {
   MNEMONIC,
-  ALCHEMY_API_KEY_GOERLI,
-  ALCHEMY_API_KEY_MUMBAI,
+  INFURA_API_KEY,
 } = process.env;
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
@@ -73,19 +72,27 @@ module.exports = {
       network_id: "*",       // Any network (default: none)
     },
     goerli: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY_GOERLI}`),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${INFURA_API_KEY}`),
       network_id: 5,       // Goerli's id
       confirmations: 0,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     mumbai: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY_MUMBAI}`),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`),
       network_id: 80001,   // Mumbai's id
       confirmations: 0,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    arbitrumGoerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://arbitrum-goerli.infura.io/v3/${INFURA_API_KEY}`), 
+      network_id: 421613,   // Arbitrum Goerli's id
+      confirmations: 0,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
     //
     // An additional network, but with some advanced options…
     // advanced: {
